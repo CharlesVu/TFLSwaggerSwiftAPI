5b4a1e20fa5e6c3bc685ae9238f557660023fdc9
Charles Vu <charles.vu@yoti.com>
2017-10-02 08:56:58 +0100

    Added compatibility to AppleTV

---
+++ TFLSwaggerClientTV/TFLSwaggerClientTV.h
@@ -0,0 +1,19 @@
+//
+//  TFLSwaggerClientTV.h
+//  TFLSwaggerClientTV
+//
+//  Created by Charles Vu on 26/09/2017.
+//  Copyright © 2017 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TFLSwaggerClientTV.
+FOUNDATION_EXPORT double TFLSwaggerClientTVVersionNumber;
+
+//! Project version string for TFLSwaggerClientTV.
+FOUNDATION_EXPORT const unsigned char TFLSwaggerClientTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TFLSwaggerClientTV/PublicHeader.h>
+
+
