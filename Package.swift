be4190548f522817b83ea0315e0afb0de9c996c8
Charles Vu <753278+CharlesVu@users.noreply.github.com>
2019-06-06 15:48:10 +0100

    Changed name to TFLApiModels and renamed files

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "TFLSwaggerSwiftAPI",
+    name: "TFLApiModels",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "TFLSwaggerSwiftAPI",
-            targets: ["TFLSwaggerSwiftAPI"]),
+            name: "TFLApiModels",
+            targets: ["TFLApiModels"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "TFLSwaggerSwiftAPI",
+            name: "TFLApiModels",
             dependencies: []),
     ]
 )
