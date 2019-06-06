6344e3e1f73a1e8e4480528b02fc39797d9fb895
Charles Vu <753278+CharlesVu@users.noreply.github.com>
2019-06-06 15:06:50 +0100

    RemovedAlamoFire from here as the swagger implementation doesn't work

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ let package = Package(
             targets: ["TFLSwaggerSwiftAPI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: Version(4, 8, 2))
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
@@ -21,6 +20,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "TFLSwaggerSwiftAPI",
-            dependencies: ["Alamofire"]),
+            dependencies: []),
     ]
 )
